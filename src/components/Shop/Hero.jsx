f22b277d247028d68f3c3fcc52789b331f8ab520
Balogun Temitope <85529178+Temitope-cloud@users.noreply.github.com>
2025-03-19 20:43:53 +0100

    Updated

--- src/components/Shop/Hero.jsx
+++ src/components/Shop/Hero.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -65,62 +68,130 @@ const Hero = () => {
     },
   ];
 
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  });
+
   return (
     <>
       <div className="mb-1 flex h-13 w-full bg-black">
         <div className="mx-5 flex gap-2 text-white">
-          <div
-            className="flex cursor-pointer items-center bg-[#333333] px-5 select-none"
-            onClick={toggleMenu}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-              id="Dashboard-3--Streamline-Core"
-              height={20}
-              width={20}
+          {isMobile ? (
+            <div className="flex cursor-pointer items-center bg-[#333333] px-5 select-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+                id="Dashboard-3--Streamline-Core"
+                height={20}
+                width={20}
+              >
+                <desc>
+                  {"Dashboard 3 Streamline Icon: https://streamlinehq.com"}
+                </desc>
+                <g id="dashboard-3--app-application-dashboard-home-layout-vertical">
+                  <path
+                    id="Vector"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13 6.5H9c-0.27614 0 -0.5 0.22386 -0.5 0.5v6c0 0.2761 0.22386 0.5 0.5 0.5h4c0.2761 0 0.5 -0.2239 0.5 -0.5V7c0 -0.27614 -0.2239 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                  <path
+                    id="Vector_2"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13 0.5H9c-0.27614 0 -0.5 0.223858 -0.5 0.5v2.01c0 0.27614 0.22386 0.5 0.5 0.5h4c0.2761 0 0.5 -0.22386 0.5 -0.5V1c0 -0.276142 -0.2239 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                  <path
+                    id="Vector_3"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 0.5H1C0.723858 0.5 0.5 0.723858 0.5 1v6c0 0.27614 0.223858 0.5 0.5 0.5h4c0.27614 0 0.5 -0.22386 0.5 -0.5V1c0 -0.276142 -0.22386 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                  <path
+                    id="Vector_4"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 10.49H1c-0.276142 0 -0.5 0.2238 -0.5 0.5V13c0 0.2761 0.223858 0.5 0.5 0.5h4c0.27614 0 0.5 -0.2239 0.5 -0.5v-2.01c0 -0.2762 -0.22386 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                </g>
+              </svg>
+              <NavLink to="/categories" className="ms-2 me-1">
+                Categories
+              </NavLink>
+            </div>
+          ) : (
+            <div
+              className="flex cursor-pointer items-center bg-[#333333] px-5 select-none"
+              onClick={toggleMenu}
             >
-              <desc>
-                {"Dashboard 3 Streamline Icon: https://streamlinehq.com"}
-              </desc>
-              <g id="dashboard-3--app-application-dashboard-home-layout-vertical">
-                <path
-                  id="Vector"
-                  stroke="white"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13 6.5H9c-0.27614 0 -0.5 0.22386 -0.5 0.5v6c0 0.2761 0.22386 0.5 0.5 0.5h4c0.2761 0 0.5 -0.2239 0.5 -0.5V7c0 -0.27614 -0.2239 -0.5 -0.5 -0.5Z"
-                  strokeWidth={1}
-                />
-                <path
-                  id="Vector_2"
-                  stroke="white"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13 0.5H9c-0.27614 0 -0.5 0.223858 -0.5 0.5v2.01c0 0.27614 0.22386 0.5 0.5 0.5h4c0.2761 0 0.5 -0.22386 0.5 -0.5V1c0 -0.276142 -0.2239 -0.5 -0.5 -0.5Z"
-                  strokeWidth={1}
-                />
-                <path
-                  id="Vector_3"
-                  stroke="white"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5 0.5H1C0.723858 0.5 0.5 0.723858 0.5 1v6c0 0.27614 0.223858 0.5 0.5 0.5h4c0.27614 0 0.5 -0.22386 0.5 -0.5V1c0 -0.276142 -0.22386 -0.5 -0.5 -0.5Z"
-                  strokeWidth={1}
-                />
-                <path
-                  id="Vector_4"
-                  stroke="white"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5 10.49H1c-0.276142 0 -0.5 0.2238 -0.5 0.5V13c0 0.2761 0.223858 0.5 0.5 0.5h4c0.27614 0 0.5 -0.2239 0.5 -0.5v-2.01c0 -0.2762 -0.22386 -0.5 -0.5 -0.5Z"
-                  strokeWidth={1}
-                />
-              </g>
-            </svg>
-            <p className="ms-2 me-1">Categories</p>
-          </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+                id="Dashboard-3--Streamline-Core"
+                height={20}
+                width={20}
+              >
+                <desc>
+                  {"Dashboard 3 Streamline Icon: https://streamlinehq.com"}
+                </desc>
+                <g id="dashboard-3--app-application-dashboard-home-layout-vertical">
+                  <path
+                    id="Vector"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13 6.5H9c-0.27614 0 -0.5 0.22386 -0.5 0.5v6c0 0.2761 0.22386 0.5 0.5 0.5h4c0.2761 0 0.5 -0.2239 0.5 -0.5V7c0 -0.27614 -0.2239 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                  <path
+                    id="Vector_2"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13 0.5H9c-0.27614 0 -0.5 0.223858 -0.5 0.5v2.01c0 0.27614 0.22386 0.5 0.5 0.5h4c0.2761 0 0.5 -0.22386 0.5 -0.5V1c0 -0.276142 -0.2239 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                  <path
+                    id="Vector_3"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 0.5H1C0.723858 0.5 0.5 0.723858 0.5 1v6c0 0.27614 0.223858 0.5 0.5 0.5h4c0.27614 0 0.5 -0.22386 0.5 -0.5V1c0 -0.276142 -0.22386 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                  <path
+                    id="Vector_4"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 10.49H1c-0.276142 0 -0.5 0.2238 -0.5 0.5V13c0 0.2761 0.223858 0.5 0.5 0.5h4c0.27614 0 0.5 -0.2239 0.5 -0.5v-2.01c0 -0.2762 -0.22386 -0.5 -0.5 -0.5Z"
+                    strokeWidth={1}
+                  />
+                </g>
+              </svg>
+              <p className="ms-2 me-1">Categories</p>
+            </div>
+          )}
+
           <div className="flex items-center bg-[#333333] px-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -151,15 +222,17 @@ const Hero = () => {
                 ></path>
               </g>
             </svg>
-            <p className="ms-2 me-1">Brand</p>
+            <NavLink to="/brand" className="ms-2 me-1">
+              Brand{" "}
+            </NavLink>
           </div>
         </div>
       </div>
       <div className="mx-5 flex h-[calc(100vh-110px)] gap-5">
         <div
-          className={`hidden w-[40%] bg-white px-3 pt-5 transition-all duration-500 sm:block ${
+          className={`hidden w-[40%] bg-white px-3 pt-5 transition-all duration-700 sm:block ${
             isOpen
-              ? "h-full opacity-100"
+              ? "h-full overflow-hidden opacity-100"
               : "hidden h-0 overflow-hidden opacity-0"
           }`}
         >
@@ -179,8 +252,8 @@ const Hero = () => {
           </ul>
         </div>
         <div
-          className={`bg-red-300 transition-all duration-500 ${isOpen ? "md:w-[60%]" : "w-full"}`}
-        ></div>{" "}
+          className={`bg-red-300 transition-all duration-500 ${isOpen ? "w-full" : "w-full"}`}
+        ></div>
       </div>
     </>
   );
